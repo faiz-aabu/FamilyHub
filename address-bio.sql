a247aa2e0e25c9f1388044773eb78d9cf8eb61a3
Faiz09 <96374817+faiz-1000@users.noreply.github.com>
2026-07-23 13:55:57 +0100

    Add Address and Bio columns to AspNetUsers

---
+++ address-bio.sql
@@ -0,0 +1,34 @@
+﻿BEGIN TRANSACTION;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260723124844_AddApplicationUserAddressAndBio'
+)
+BEGIN
+    ALTER TABLE [AspNetUsers] ADD [Address] nvarchar(max) NULL;
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260723124844_AddApplicationUserAddressAndBio'
+)
+BEGIN
+    ALTER TABLE [AspNetUsers] ADD [Bio] nvarchar(max) NULL;
+END;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM [__EFMigrationsHistory]
+    WHERE [MigrationId] = N'20260723124844_AddApplicationUserAddressAndBio'
+)
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260723124844_AddApplicationUserAddressAndBio', N'8.0.22');
+END;
+GO
+
+COMMIT;
+GO
+
